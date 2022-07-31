21a3eb2b533bbde26fd532a4188b1adf25742cc8
Natalia <gurova_nata22@mail.ru>
2022-07-31 22:32:31 +0300

    Add Zadacha13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,23 @@
+﻿void Zadacha13()
+{
+    //Напишите программу, которая с помощью деления выводит третью цифру
+    //заданного числа или сообщает, что третьей цифры нет.
+    Console.Write("Введите любое число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    number = Math.Abs(number);
+
+    if (number < 100)
+    {
+        Console.Write("В числе менее трех цифр");
+    }
+    else
+    {
+        while (number > 999)
+        {
+            number/=10;
+        }
+        Console.Write("Третья цифра числа: " + number%10);
+    }
+}
+
+Zadacha13();
