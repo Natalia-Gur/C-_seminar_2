9a3966633e3480c58bf72a329e4ee83c92f91656
Natalia <gurova_nata22@mail.ru>
2022-07-31 21:29:46 +0300

    Add Zadacha10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,14 @@
+﻿void Zadacha10()
+{
+    //Напишите программу, которая принимает на вход трёхзначное число
+    // и на выходе показывает вторую цифру этого числа.
+
+    Random rand = new Random();
+    int number = rand.Next(100,1000);
+    Console.WriteLine(number);
+
+    int tens = number/10%10;
+    Console.WriteLine("Вторая цифра числа: " + tens);
+}
+
+Zadacha10();
